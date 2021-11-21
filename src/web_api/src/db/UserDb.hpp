0067f1799d8108511200737f2a4ebb92a1525898
roleyzhang <roleyzhang@gmail.com>
2021-11-20 23:43:59 -0800

    fixed web-api ccls issues

--- src/web_api/src/db/UserDb.hpp
+++ src/web_api/src/db/UserDb.hpp
@@ -5,26 +5,23 @@
 #include "dto/UserDto.hpp"
 #include "oatpp-sqlite/orm.hpp"
 
-#include OATPP_CODEGEN_BEGIN(DbClient) //<- Begin Codegen
+#include OATPP_CODEGEN_BEGIN(DbClient)  //<- Begin Codegen
 
 /**
  * UserDb client definitions.
  */
 class UserDb : public oatpp::orm::DbClient {
-public:
-
+ public:
   UserDb(const std::shared_ptr<oatpp::orm::Executor>& executor)
-    : oatpp::orm::DbClient(executor)
-  {
-
+      : oatpp::orm::DbClient(executor) {
     oatpp::orm::SchemaMigration migration(executor);
-    migration.addFile(1 /* start from version 1 */, DATABASE_MIGRATIONS "/001_init.sql");
+    migration.addFile(1 /* start from version 1 */,
+                      DATABASE_MIGRATIONS "/001_init.sql");
     // TODO - Add more migrations here.
-    migration.migrate(); // <-- run migrations. This guy will throw on error.
+    migration.migrate();  // <-- run migrations. This guy will throw on error.
 
     auto version = executor->getSchemaVersion();
     OATPP_LOGD("UserDb", "Migration - OK. Version=%lld.", version);
-
   }
 
   QUERY(createUser,
@@ -44,21 +41,16 @@ public:
         " id=:user.id;",
         PARAM(oatpp::Object<UserDto>, user))
 
-  QUERY(getUserById,
-        "SELECT * FROM AppUser WHERE id=:id;",
+  QUERY(getUserById, "SELECT * FROM AppUser WHERE id=:id;",
         PARAM(oatpp::Int32, id))
 
-  QUERY(getAllUsers,
-        "SELECT * FROM AppUser LIMIT :limit OFFSET :offset;",
-        PARAM(oatpp::UInt32, offset),
-        PARAM(oatpp::UInt32, limit))
+  QUERY(getAllUsers, "SELECT * FROM AppUser LIMIT :limit OFFSET :offset;",
+        PARAM(oatpp::UInt32, offset), PARAM(oatpp::UInt32, limit))
 
-  QUERY(deleteUserById,
-        "DELETE FROM AppUser WHERE id=:id;",
+  QUERY(deleteUserById, "DELETE FROM AppUser WHERE id=:id;",
         PARAM(oatpp::Int32, id))
-
 };
 
-#include OATPP_CODEGEN_END(DbClient) //<- End Codegen
+#include OATPP_CODEGEN_END(DbClient)  //<- End Codegen
 
-#endif //CRUD_USERDB_HPP
+#endif  // CRUD_USERDB_HPP
